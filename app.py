63392e176d2037baef99a217b7d75c53595f19ff
Harleen <harleenbbagga@gmail.com>
2023-11-12 22:58:00 +0530

    Added necessary access key and integrated essential libraries

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from langchain.document_loaders import TextLoader
+from langchain.vectorstores import FAISS
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from dotenv import load_dotenv
+
+load_dotenv()
