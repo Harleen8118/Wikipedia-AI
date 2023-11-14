c07f62cdcf501e5dd346bfcce1d308cb5856166f
Harleen <harleenbbagga@gmail.com>
2023-11-15 00:14:44 +0530

    Function for similarity search

--- app.py
+++ app.py
@@ -8,3 +8,20 @@ from langchain.chains import LLMChain
 from dotenv import load_dotenv
 
 load_dotenv()
+
+# 1. loading txt file and dividing it into chunks
+loader = TextLoader("./wikipedia_AI.txt", encoding='utf-8')
+documents = loader.load_and_split()
+
+embeddings = OpenAIEmbeddings()
+db = FAISS.from_documents(documents, embeddings)
+
+# 2. Function for similarity search
+def retrieve_info(query):
+    similar_response = db.similarity_search(query, k=5)
+
+    page_contents_array = [doc.page_content for doc in similar_response]
+
+    print(page_contents_array)
+
+    return page_contents_array
